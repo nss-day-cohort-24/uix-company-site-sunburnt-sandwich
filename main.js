bd84ae14e2be35ff3ca767ab505d6fe136dde9b3
Miranda Simpson <mirandasimpson@Mirandas-MacBook-Air.local>
2018-01-11 10:26:33 -0600

    added some comments

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+//this one holds the info, makes a sting with it, and puts it into HTML
 function productFunc() {
     var product = ["name", " decription", " price", " <a href=''>Image</a>"];
     product.toString();
     document.getElementById("elementId").innerHTML = product;
 }
+//this one does nothing for a onMouseout event
 function nothing() {
 	document.getElementById("elementId").innerHTML = "";
 }
