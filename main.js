1d75faf81ed44bc0340c7e6285224765f8ccafba
Miranda Simpson <mirandasimpson@Mirandas-MacBook-Air.local>
2018-01-11 09:08:33 -0600

    the product functions

---
+++ main.js
@@ -0,0 +1,8 @@
+function theEarpiece() {
+    var earpiece = ["name", " decription", " price", " <a href=''>Image</a>"];
+    earpiece.toString();
+    document.getElementById("demo").innerHTML = earpiece;
+}
+function nothing() {
+	document.getElementById("demo").innerHTML = "";
+}
