6093938ff9fa755143b092925fadc15ec051ad94
Miranda Simpson <mirandasimpson@Mirandas-MacBook-Air.local>
2018-01-11 10:41:58 -0600

    add contact and js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-//this one holds the info, makes a sting with it, and puts it into HTML
+//this one holds the info, makes a sting with it, and puts it into HTML//
 function productFunc() {
     var product = ["name", " decription", " price", " <a href=''>Image</a>"];
     product.toString();
     document.getElementById("elementId").innerHTML = product;
 }
-//this one does nothing for a onMouseout event
 function nothing() {
 	document.getElementById("elementId").innerHTML = "";
 }
