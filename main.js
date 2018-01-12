2b182d4be8d738914be2acd1a1c3a945cdecd309
Miranda Simpson <mirandasimpson@Mirandas-Air.attlocal.net>
2018-01-12 12:42:27 -0600

    flips and JS for products page

--- main.js
+++ main.js
@@ -1,8 +1,85 @@
-function theEarpiece() {
-    var earpiece = ["name", "<br>" + " decription","<br>" + " price","<br>" + " <a href='www.w3schools.com'>Image</a>"];
+
+////////watch function
+function watch() {
+    var earpiece = ["name", "decription", "price"];
     earpiece.toString();
     document.getElementById("watch").innerHTML = earpiece;
 }
-function nothing() {
+function nonWatch() {
 	document.getElementById("watch").innerHTML = "";
+}
+
+
+///////ironman function
+function ironman() {
+    var earpiece = ["name", "decription", "price"];
+    earpiece.toString();
+    document.getElementById("ironman").innerHTML = earpiece;
+}
+function nonIronman() {
+	document.getElementById("ironman").innerHTML = "";
+}
+
+
+///////ironComp function
+function ironComp() {
+    var earpiece = ["name", "decription", "price"];
+    earpiece.toString();
+    document.getElementById("ironComp").innerHTML = earpiece;
+}
+function nonIronComp() {
+	document.getElementById("ironComp").innerHTML = "";
+}
+
+
+////////jarvis function
+function jarvis() {
+    var earpiece = ["name", "decription", "price"];
+    earpiece.toString();
+    document.getElementById("jarvis").innerHTML = earpiece;
+}
+function nonJarvis() {
+	document.getElementById("jarvis").innerHTML = "";
+}
+
+
+///////macbook function
+function macBook() {
+    var earpiece = ["name", "decription", "price"];
+    earpiece.toString();
+    document.getElementById("macbook").innerHTML = earpiece;
+}
+function nonMacBook() {
+	document.getElementById("macbook").innerHTML = "";
+}
+
+
+///////glasses function
+function glasses() {
+    var earpiece = ["name", "decription", "price"];
+    earpiece.toString();
+    document.getElementById("glasses").innerHTML = earpiece;
+}
+function nonGlasses() {
+	document.getElementById("glasses").innerHTML = "";
+}
+
+////////ring function
+function ring() {
+    var earpiece = ["name", "decription", "price"];
+    earpiece.toString();
+    document.getElementById("ring").innerHTML = earpiece;
+}
+function nonRing() {
+	document.getElementById("ring").innerHTML = "";
+}
+
+////////tablet function
+function tablet() {
+    var earpiece = ["name", "decription", "price"];
+    earpiece.toString();
+    document.getElementById("tablet").innerHTML = earpiece;
+}
+function nonTablet() {
+	document.getElementById("tablet").innerHTML = "";
 }
